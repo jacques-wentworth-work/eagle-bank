165f973fc7539c8d562fa4e6421417a60e9fe0d7
Jacques Wentworth <jacques.wentworth@kindredgroup.com>
2025-07-23 07:51:04 +0100

    cleanup

--- src/test/java/com/eaglebank/integration/UserControllerIntegrationTest.java
+++ src/test/java/com/eaglebank/integration/UserControllerIntegrationTest.java
@@ -11,7 +11,6 @@ import com.eaglebank.resource.UserCreateRequest;
 import com.eaglebank.service.jwt.JwtService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
